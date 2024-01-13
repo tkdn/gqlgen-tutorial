f29a657772abf2ca8030828582dec5d6c59eec07
tkdn <tkdnation@gmail.com>
2024-01-13 15:25:39 +0900

    not eagerly fetch the user, so changes todo model and gen codes.
    - https://gqlgen.com/getting-started/#dont-eagerly-fetch-the-user

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,5 +1,6 @@
 package graph
 
+//go:generate go run github.com/99designs/gqlgen generate
 import "github.com/tkdn/gqlgen-tutorial/graph/model"
 
 // This file will not be regenerated automatically.
